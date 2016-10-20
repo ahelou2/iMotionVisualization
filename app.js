f44725e674d4915ac72abca9957a6deb6ee6c1e3
Akram <ahelou2@hotmail.com>
2016-10-19 17:32:22 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/', require('redirect-https')({
+//   body: '<!-- Hello Mr Developer! Please use HTTPS instead -->',
+//   port: 3000,
+//   trustProxy: true,
+// }));
+ 
+var routes = require("./routes/routes.js")(app);
+ 
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
