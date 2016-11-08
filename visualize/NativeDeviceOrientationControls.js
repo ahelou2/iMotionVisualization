e79b18ae773a7770aa0a79f677c460d7305ab571
Akram <ahelou2@hotmail.com>
2016-11-07 16:07:08 -0800

    done: moving camera forward using touchscreen

--- visualize/NativeDeviceOrientationControls.js
+++ visualize/NativeDeviceOrientationControls.js
@@ -9,6 +9,10 @@ THREE.NativeDeviceOrientationControls = function( object ) {
 
 	this.enabled = true;
 
+	this.moveState = {forward: 0};
+	this.movementSpeed = 100.0;
+	this.moveVector = new THREE.Vector3( 0, 0, 0 );
+
 
 	// The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
 
@@ -21,29 +25,73 @@ THREE.NativeDeviceOrientationControls = function( object ) {
 		var q0 = new THREE.Quaternion();
 
 		var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
-		// var q1 = new THREE.Quaternion(0, Math.sqrt( 0.5 ), 0, Math.sqrt( 0.5 ) ); // - PI/2 around the y-axis
 
-		return function( quaternion, alpha, beta, gamma, orient, newQuaternion ) {
+		return function( quaternion, alpha, beta, gamma, orient) {
 
-			// eulerAngles: {pitch: number, roll: number, yaw: number},
-
-			// euler.set( beta, -gamma, alpha, 'YXZ' ); // 'XYZ' for the device, but 'YXZ' for us
 			euler.set( beta, alpha, -gamma, 'YXZ' ); // 'XYZ' for the device, but 'YXZ' for us
 			
 			quaternion.setFromEuler( euler ); // orient the device
-			// quaternion.copy(newQuaternion);
 
 			quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
-
-			// quaternion.multiply( q0.setFromAxisAngle( zee, orient ) ); // adjust for screen orientation
 
 		}
 
 	}();
 
+
+
 	this.connect = function() {
 
 		scope.enabled = true;
+
+		// var _processTouchOnScreen = bind( this, this.processTouchOnScreen );
+		// var _processTouchEnded = bind( this, this.processTouchEnded );
+
+		var _processTouchOnScreen = this.processTouchOnScreen.bind(this);
+		
+		var _processTouchEnded = this.processTouchEnded.bind(this);
+		// Register touch event handlers
+		window.addEventListener('touchstart', _processTouchOnScreen, false);
+		window.addEventListener('touchmove', _processTouchOnScreen, false);
+		window.addEventListener('touchcancel', _processTouchEnded, false);
+		window.addEventListener('touchend', _processTouchEnded, false);
+
+	};
+
+	this.disconnect = function() {
+
+
+		window.removeEventListener('touchstart', _processTouchOnScreen, false);
+		window.removeEventListener('touchmove', _processTouchOnScreen, false);
+		window.removeEventListener('touchcancel', _processTouchEnded, false);
+		window.removeEventListener('touchend', _processTouchEnded, false);
+
+		scope.enabled = false;
+
+	};
+
+	this.processTouchOnScreen = function( event ) {
+
+		console.log("processTouchOnScreen");
+
+		this.moveState.forward = -1; // -1 bc that is the direction forward (from screen to back of phone)
+
+
+		this.updateMovementVector();
+	};
+
+	this.processTouchEnded = function( event ) {
+
+		console.log("processTouchEnded");
+
+		this.moveState.forward = 0;
+		this.updateMovementVector();
+
+	};
+
+	this.updateMovementVector = function() {
+
+		this.moveVector.z = this.moveState.forward;
 
 	};
 
@@ -52,25 +100,27 @@ THREE.NativeDeviceOrientationControls = function( object ) {
 
 		var attitude = interpretData(buffer.read());
 		if (attitude ==  null) {
-			// this.update();
+
 			return;
 		}
 
 		var eulerAngles = attitude.eulerAngles;
-		var quaternion = new THREE.Quaternion(attitude.quaternion.x, attitude.quaternion.y, attitude.quaternion.z, attitude.quaternion.w);
-		// quaternion = quaternion.inverse();
-		// quaternion.set(attitude.quaternion.z, attitude.quaternion.y, attitude.quaternion.x, attitude.quaternion.w);
-		// var quaternion = attitude.quaternion;
 
-		
 
 		if ( scope.enabled === false ) return;
 
 		var orient = Math.PI / 2;
 		orient = eulerAngles.yaw;
 
-		setObjectQuaternion( scope.object.quaternion, eulerAngles.yaw, eulerAngles.pitch, eulerAngles.roll, orient, quaternion );
+		setObjectQuaternion( scope.object.quaternion, eulerAngles.yaw, eulerAngles.pitch, eulerAngles.roll, orient);
 
+		scope.object.translateZ( this.moveVector.z * this.movementSpeed );
+
+	};
+
+	this.dispose = function() {
+
+		this.disconnect();
 	};
 
 	this.connect();
